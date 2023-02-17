bdaa13fa2fb539017445073009391737a1514b93
hichambouzid <bouzidhicham51@gmail.com>
2023-02-17 15:44:01 -0800

    print numbes type int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9 using putchar
+ *              while using int variable
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		putchar(i + 48);
+		i++;
+	}
+	putchar('\n');
+}
