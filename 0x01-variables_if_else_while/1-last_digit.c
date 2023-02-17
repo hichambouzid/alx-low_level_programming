d4e32d234d9075384ba81458858193a74ba079d6
hichambouzid <bouzidhicham51@gmail.com>
2023-02-17 11:04:24 -0800

    commit the last number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/* the last number */
+int main(void)
+{
+	int n;
+	int last_number;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_number = n % 10;
+	if (last_number > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_number);
+	else if (n < 6 && n != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,last_number);
+	else if (last_number == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_number);
+	return (0);
+}
