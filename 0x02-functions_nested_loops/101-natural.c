bcfa0c63b63628664cedb90d9e92695452e316bc
hichambouzid <bouzidhicham51@gmail.com>
2023-02-22 14:51:26 -0800

    commit the sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * counte_multiples - count the sum of multiples 5 and 6
+ *
+ * Rentun: Alway void
+ */
+void counte_multiples(void)
+{
+	int i;
+	int result;
+
+	i = 2;
+	result = 0;
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			result += i;
+		if (i % 5 == 0)
+			result += i;
+		i++;
+	}
+	printf("%d\n", result);
+}
+/**
+ * main - function for execution
+ *
+ * Return: always return 0.
+ */
+int main(void)
+{
+	counte_multiples();
+	return (0);
+}
