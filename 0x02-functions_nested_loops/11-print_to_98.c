58f91ca8fd4b3495aa462ec50cf6b3f564cd82a2
hichambouzid <bouzidhicham51@gmail.com>
2023-02-22 10:11:46 -0800

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print all numbers from n to 98
+ *
+ * @n: input give them
+ *
+ * Return: always return nothing
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+		printf("%d\n", n);
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d\n", n);
+	}
+}
