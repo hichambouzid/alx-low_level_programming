c669045602bd9d0f4e98a04a2dc46805e536ba95
hichambouzid <bouzidhicham51@gmail.com>
2023-02-23 05:45:26 -0800

    commi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function multiplies tow integers
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: Alwas return the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
