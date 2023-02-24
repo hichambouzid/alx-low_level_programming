706cea5ccf82037bdc803f5634e0330a20a8fc56
hichambouzid <bouzidhicham51@gmail.com>
2023-02-24 13:00:38 -0800

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,20 @@
  */
 void print_number(int n)
 {
-
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar(2 + 48);
+		_putchar(1 + 48);
+		_putchar(4 + 48);
+		_putchar(7 + 48);
+		_putchar(4 + 48);
+		_putchar(8 + 48);
+		_putchar(3 + 48);
+		_putchar(6 + 48);
+		_putchar(4 + 48);
+		_putchar(8 + 48);
+	}
 	if (n < 0)
 	{
 		_putchar('-');
