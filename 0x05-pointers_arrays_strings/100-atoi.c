71b325bc09e004743f34aba69fadf27649380218
hichambouzid <bouzidhicham51@gmail.com>
2023-02-27 13:59:54 -0800

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,19 +10,23 @@ int _atoi(char *s)
 	int i;
 	int res;
 	int sign;
+	int lenght;
 
+	lenght = 0;
 	res = 0;
 	i = 0;
 	sign = 1;
 	if (s[0] == '\0')
 		return (res);
+	while (s[lenght])
+		lenght++;
 	while (s[i] < '0' || s[i] > '9')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	if ((s[i] == '\0') || (s[i] == '\n'))
+	if (i == lenght)
 		return (0);
 	while (s[i] >= '0' && s[i] <= '9')
 	{
