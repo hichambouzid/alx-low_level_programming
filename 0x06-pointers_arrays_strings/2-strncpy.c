b581994afcdc3e191e7e17e0287afb6cdcb9a95f
hichambouzid <bouzidhicham51@gmail.com>
2023-03-01 11:12:02 -0800

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int lenght;
 
 	i = 0;
-	lenght = 0;
-	while (src[lenght])
-		lenght++;
-	while (i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
+	}
+	if (src[i] == '\0')
+	{
+		dest[i] = '\0';
+		return (dest);
 	}
 	return (dest);
 }
