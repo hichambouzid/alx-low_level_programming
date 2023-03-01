77a2f69215086b37324260ac1e6e18ef0e2fe4a7
hichambouzid <bouzidhicham51@gmail.com>
2023-03-01 15:42:04 -0800

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,6 +19,26 @@ char *cap_string(char *str)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] += -32;
 		}
+		else if ((str[i] == ',') || (str[i] == ';') || (str[i] == '.'))
+		{
+			if (str[i + 1] >= 'a' && str[i] <= 'z')
+				str[i + 1] += -32;
+		}
+		else if ((str[i] == '!') || (str[i] == '?') || (str[i] == '"'))
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] += -32;
+		}
+		else if ((str[i] == '(') || (str[i] == ')') || (str[i] == '{'))
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] += -32;
+		}
+		else if (str[i] == '}')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] += -32;
+		}
 		i++;
 	}
 	return (str);
