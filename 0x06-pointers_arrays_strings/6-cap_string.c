0ebe338e0a89a5f03612de19826b0eca96d60767
hichambouzid <bouzidhicham51@gmail.com>
2023-03-01 19:20:00 -0800

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 /**
  * cap_string - the first char is uppercase
  *
- * @str: input
+ * @s: input
  *
  * Return: output is a pointer
  */
@@ -10,12 +10,12 @@ char *cap_string(char *s)
 	int i;
 
 	i = 0;
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s += -32;
 	while (s[i])
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
+			if (i == 0)
+				s[i] -= 32;
 			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
 					s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
 					s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
