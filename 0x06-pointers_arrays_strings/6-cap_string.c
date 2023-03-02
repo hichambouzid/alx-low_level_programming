4201c69652b0eba84168c538ca9d346777fd8613
hichambouzid <bouzidhicham51@gmail.com>
2023-03-01 19:16:58 -0800

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,41 +5,27 @@
  *
  * Return: output is a pointer
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i;
 
 	i = 0;
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str += -32;
-	while (str[i])
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s += -32;
+	while (s[i])
 	{
-		if ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n'))
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] += -32;
-		}
-		else if ((str[i] == ',') || (str[i] == ';') || (str[i] == '.'))
-		{
-			if (str[i + 1] >= 'a' && str[i] <= 'z')
-				str[i + 1] += -32;
-		}
-		else if ((str[i] == '!') || (str[i] == '?') || (str[i] == '"'))
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] += -32;
-		}
-		else if ((str[i] == '(') || (str[i] == ')') || (str[i] == '{'))
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] += -32;
-		}
-		else if (str[i] == '}')
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] += -32;
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+					s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+					s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+					s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+					s[i - 1] == 124)
+			{
+				s[i] += -32;
+			}
 		}
 		i++;
 	}
-	return (str);
+	return (s);
 }
