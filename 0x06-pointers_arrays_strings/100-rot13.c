28c493cc9e4657f239af9b1c449b1b235b281cd8
hichambouzid <bouzidhicham51@gmail.com>
2023-03-02 17:06:26 -0800

    lots of conditions

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,15 +10,16 @@ char *rot13(char *s)
 	int i;
 	int j;
 
-	i = 0;
-	char chars[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+	char ca[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
 	char chare[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+
+	i = 0;
 
 	while (s[i])
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == chars[j])
+			if (s[i] == ca[j])
 			{
 				s[i] = chare[j];
 				break;
