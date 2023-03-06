9da6f6810604ff7720b4c4b655be437bda7fdbb9
hichambouzid <bouzidhicham51@gmail.com>
2023-03-06 09:51:38 -0800

    it's like function strcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _memcpy - function
+ *
+ * @dest: input
+ * @src: second input
+ * @n: number
+ *
+ * Return: always return pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+
+}
