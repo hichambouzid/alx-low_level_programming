f3619c57a4bc7e89d4c6395eac0c7117b40bcb0a
hichambouzid <bouzidhicham51@gmail.com>
2023-03-06 11:24:54 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+/**
+ * _strspn - function
+ *
+ * @s: string
+ * @accept: string
+ *
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int res;
+
+	res = 0;
+	i = 0;
+	j = 0;
+	while (accept[i])
+	{
+		for (j = 0; s[j]; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				res += 1;
+				break;
+			}
+		}
+		i++;
+	}
+	return (res + 1);
+}
