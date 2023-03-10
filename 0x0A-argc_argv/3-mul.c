db935b22a0eae80b85de0f08e432e6b86d6fa949
hichambouzid <bouzidhicham51@gmail.com>
2023-03-10 15:52:02 -0800

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - function
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: always 0;
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
