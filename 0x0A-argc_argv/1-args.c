d82d0d413320b680a37b0ffa3dbd95735e385500
hichambouzid <bouzidhicham51@gmail.com>
2023-03-10 11:35:09 -0800

    print number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - second prototype of main
+ *
+ * @argc: number of argument
+ *
+ * @argv: string
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	_putchar(argc + 47);
+	_putchar('\n');
+	return (0);
+}
