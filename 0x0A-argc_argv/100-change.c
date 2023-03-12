dac4d33be25f6d0983491ff009d6b256822e670a
hichambouzid <bouzidhicham51@gmail.com>
2023-03-12 08:23:28 -0700

    make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -55,13 +55,15 @@ int change(int i)
 int main(int argc, char **argv)
 {
 	int i;
+	int j;
 
+	j = 0;
 	if (argc == 2)
 	{
 		i = atoi(argv[1]);
 		if (i < 0)
 		{
-			printf("0\n");
+			printf("%d\n", j);
 			return (0);
 		}
 		printf("%d\n", change(i));
