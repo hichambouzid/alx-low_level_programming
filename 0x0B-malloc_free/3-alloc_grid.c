868d4e750fda1c57dd28b188370db00281a23201
hichambouzid <bouzidhicham51@gmail.com>
2023-03-15 06:51:05 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include<stdlib.h>
+/**
+ * alloc_grid - creat an array in heap
+ *
+ * @width: input
+ * @height: input
+ *
+ * Return: apointer to a pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **ptr;
+
+	if ((width * height) <= 0)
+		return (NULL);
+
+	ptr = (int **)malloc(sizeof(int) * (height));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *)malloc(sizeof(int) * (width));
+		if (ptr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+}
