f74c8901588396e89ebecb1ea30ac7284c7b3713
hichambouzid <bouzidhicham51@gmail.com>
2023-03-15 13:34:06 -0700

    commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include<stdlib.h>
+/**
+ * argstostr - function concatenaties all the argumments
+ *
+ * @ac: size
+ * @av: array
+ *
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int res;
+	char *ptr;
+	int e;
+
+	res = ac;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			res += 1;
+
+	ptr = malloc(sizeof(int) * (res + 1));
+	ptr[0] = '\0';
+
+	for (i = 1; i < ac; i++)
+	{
+		e = 0;
+		while (ptr[e])
+			e++;
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[e] = av[i][j];
+			e++;
+		}
+		ptr[e] = '\n';
+		ptr[e + 1] = '\0';
+	}
+	return (ptr);
+}
