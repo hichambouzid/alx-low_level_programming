8c38bd08396599f6e698b059c1dd06375a0e12e0
hichambouzid <bouzidhicham51@gmail.com>
2023-03-14 19:19:16 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,9 +15,11 @@ int **alloc_grid(int width, int height)
 
 	if ((width * height) <= 0)
 		return (NULL);
+
 	ptr = (int **)malloc(sizeof(int) * (height));
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = (int *)malloc(sizeof(int) * (width));
@@ -32,5 +34,6 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
+
 	return (ptr);
 }
