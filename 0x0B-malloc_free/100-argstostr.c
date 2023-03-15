7b8d253b626070d5a8de01757699ea53e10bc4ac
hichambouzid <bouzidhicham51@gmail.com>
2023-03-15 13:45:18 -0700

    commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,14 +19,14 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 		for (j = 0; av[i][j] != '\0'; j++)
 			res += 1;
 
 	ptr = malloc(sizeof(int *) * (res + i + 1));
 	ptr[0] = '\0';
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		e = 0;
 		while (ptr[e])
