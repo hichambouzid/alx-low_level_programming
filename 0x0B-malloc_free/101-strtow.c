d4afaa1f7e07e6fcfe001752587684b8fa43949e
hichambouzid <bouzidhicham51@gmail.com>
2023-03-16 07:29:05 -0700

    commit

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,52 @@
+#include<stdlib.h>
+
+/**
+ * strtow - function
+ *
+ * @str: string
+ *
+ * Return: double pointer
+ */
+char **strtow(char *str)
+{
+	int i;
+	int j;
+	int e;
+	char **ptr;
+	int a;
+	int res;
+
+	e = 0;
+	j = 0;
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+			j += 1;
+		}
+	}
+	ptr = malloc(sizeof(char *) * (i));
+	if ((!ptr) || (str == NULL) || (*str == '\0'))
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j = 0;
+		if (str[i] != ' ')
+		{
+			while (str[i + j] != '\0' && str[i + j] != ' ')
+				j++;
+			ptr[e] = malloc(sizeof(char) * (j + 1));
+			if (ptr[e] == NULL)
+				return (NULL);
+			res = i + j;
+			a = 0;
+			for (; i < res; i++)
+				ptr[e][a++] = str[i];
+			ptr[e][a] = '\0';
+			e++;
+		}
+	}
+	return (ptr);
+}
