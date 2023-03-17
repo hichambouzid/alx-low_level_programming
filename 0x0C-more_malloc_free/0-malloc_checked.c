d6c4c67005b55dc37b9b20cda9464555e33b922a
hichambouzid <bouzidhicham51@gmail.com>
2023-03-17 07:17:46 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,12 @@
 #include<stdlib.h>
-
+/**
+ * malloc_checked - function contrat any type of varible and
+ * use function exit if we have an error in allocation
+ *
+ * @b: prisent the size of the variable give them in argument
+ *
+ * Return: return variable pointer type void or exit befor if we have a problem
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *i;
