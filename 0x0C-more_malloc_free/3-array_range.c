cb810525f82150a119ae73345c286f12abf998b4
hichambouzid <bouzidhicham51@gmail.com>
2023-03-17 16:15:26 -0700

    commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+/**
+ * array_range - function creat an array size from min to max
+ *
+ * @min: input
+ * @max: input
+ *
+ * Return: array
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	i = 0;
+	if (min >= max)
+		return (NULL);
+	ptr = malloc(sizeof(int) * (max - min));
+	if (!ptr)
+		return (NULL);
+	while (min < max)
+	{
+		ptr[i] = min;
+		min++;
+		i++;
+	}
+	return (ptr);
+}
