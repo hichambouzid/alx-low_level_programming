0c1796aa6b1568e536239e660bc77087f4cf6916
hichambouzid <bouzidhicham51@gmail.com>
2023-03-17 12:21:19 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,17 +15,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i = 0;
 	j = 0;
-	if ((s1 == NULL && i == 0))
-		return (NULL);
+	if ((s1 == NULL && s2 == NULL))
+	{
+		ptr = malloc(1);
+		ptr[0] = '\0';
+		return (ptr);
+	}
 
 	if (s1 == NULL)
-		s1[0] = '\0';
+		s1 = '\0';
 	if (s2 == NULL)
-		s2[0] = '\0';
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
+		s2 = '\0';
+	if (s1 != NULL)
+	{
+		while (s1[i])
+			i++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[j])
+			j++;
+	}
 	if (j <= n)
 		n = j;
 	ptr = malloc(sizeof(char) * (i + n + 1));
