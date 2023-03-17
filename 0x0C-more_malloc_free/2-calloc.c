2e4fc8a0709447a970e0f7ea226ad98d98a6bd2c
hichambouzid <bouzidhicham51@gmail.com>
2023-03-17 15:49:47 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	i = calloc(nmemb, sizeof(size));
+	i = calloc(nmemb, size);
 	if (!i)
 		exit(98);
 	return (i);
