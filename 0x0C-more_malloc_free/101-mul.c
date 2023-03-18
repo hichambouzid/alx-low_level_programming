880aa04c3d04cdf86e6c534a3f6f32b87604d5a5
hichambouzid <bouzidhicham51@gmail.com>
2023-03-18 11:48:23 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	mul = atol(argv[1] * argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }
