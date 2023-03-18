19fca47b1884433f6339613f876f7e475a348482
hichambouzid <bouzidhicham51@gmail.com>
2023-03-18 11:09:21 -0700

    commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,53 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * check - function
+ *
+ * @str: string
+ *
+ * Return: return 0 or 1
+ */
+int check(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] > '9' || str[i] < '0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/**
+ * main - second prototype of function main
+ *
+ * @argc: number of argruments
+ *
+ * @argv: input
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((check(argv[1]) == 1) || (check(argv[2]) == 1))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+		return (0);
+	}
+}
