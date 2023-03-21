4f94528590a8bceee15a85a659b0fcb3d488714e
hichambouzid <bouzidhicham51@gmail.com>
2023-03-21 15:28:32 -0700

    function return typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *strang;
+	int i, j;
 
+	i = 0;
+	j = 0;
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
 	strang = malloc(sizeof(dog_t));
-	strang->name = name;
+	strang->name = malloc(i + 1);
+	strang->owner = malloc(j + 1);
+	if (!(strang->name) || !(strang->owner))
+	{
+		free(strang);
+		return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+		strang->name[i] = name[i];
+	strang->name[i] = '\0';
+	for (j = 0; owner[j] != '\0'; j++)
+		strang->owner[j] = owner[j];
+	strang->owner[j] = '\0';
 	strang->age = age;
-	strang->owner = owner;
-
 	return (strang);
 }
