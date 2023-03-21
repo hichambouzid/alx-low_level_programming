ea0b111b33aa87a0217d388be31caa665faa29b7
hichambouzid <bouzidhicham51@gmail.com>
2023-03-21 13:11:32 -0700

    A float cannot be never null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,8 +15,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		if (d->owner == NULL)
