c0b7593affd870a3837276a64d2317412dfad6e0
hichambouzid <bouzidhicham51@gmail.com>
2023-03-22 15:30:01 -0700

    pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - function rempleace the input name the pointer to function
+ * f
+ * @f: pointer to function
+ * @name: input to reampleace to function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
