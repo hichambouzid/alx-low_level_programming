2b5e94cc0ab84c268e99f0ae3071127849d9a234
hichambouzid <bouzidhicham51@gmail.com>
2023-03-23 14:36:28 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,38 +1,32 @@
 #include "3-calc.h"
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - function to start the code
- *
- * @argc: size of command line
- * @argv: array of command
- *
- * Return: 0
- */
-int main(int argc, char **argv)
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
 {
-	int i;
-	int (*f)(int, int);
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((get_op_func(argv[2]) == NULL || argv[2][1] != '\0'))
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') &&
-			(atoi(argv[3]) == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	f = get_op_func(argv[2]);
-	i = f(atoi(argv[1]), atoi(argv[3]));
 
-	printf("%d\n", i);
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,75 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * op_add - function add tow ints
- *
- * @a: first input
- * @b: second input
- *
- * Return: number
- */
+  * op_add - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - function substract tow numbers
- *
- * @a: first input
- * @b: second input
- *
- * Return: number
- */
+  * op_sub - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - function multiplicat tow ints
- *
- * @a: first input
- * @b: secon input
- *
- * Return: number
- */
+  * op_mul - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - function divis tow numbers
- *
- * @a: first input
- * @b: second input
- *
- * Return: number
- */
+  * op_div - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
+
 /**
- * op_mod - function return modulo of tow ints
- *
- * @a: first input
- * @b: seconf input
- *
- * Return: number
- */
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
