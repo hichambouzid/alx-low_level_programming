b330a2dd0a8a416a2cd3d045f2b2b96465df6072
hichambouzid <bouzidhicham51@gmail.com>
2023-03-23 14:32:21 -0700

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') &&
-		       (atoi(argv[1]) == 0 || atoi(argv[3]) == 0))
+			(atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
