195fc403a3b94c0f7b8c9c82b97c4cfb0a567b49
hichambouzid <bouzidhicham51@gmail.com>
2023-03-23 11:04:03 -0700

    challenge code

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - function to start the code
+ *
+ * @argc: size of command line
+ * @argv: array of command
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '/' && argv[2][0] != '+' && argv[2][0] != '-'
+			&& argv[2][0] != '*' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') &&
+		       (atoi(argv[1]) == 0 || atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	f = get_op_func(argv[2]);
+	i = f(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", i);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - function add tow ints
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function substract tow numbers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: number
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function multiplicat tow ints
+ *
+ * @a: first input
+ * @b: secon input
+ *
+ * Return: number
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function divis tow numbers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: number
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function return modulo of tow ints
+ *
+ * @a: first input
+ * @b: seconf input
+ *
+ * Return: number
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
