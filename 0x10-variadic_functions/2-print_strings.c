b412cba0f30dafc0b84ab961db299c75dd74abb1
hichambouzid <bouzidhicham51@gmail.com>
2023-03-24 19:06:50 -0700

    print a strings with variadic function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include<stdarg.h>
+#include<stdio.h>
+/**
+ * print_strings - print a strings passed as parametre
+ *
+ * @separator: input
+ * @n: number of agrument
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+	char *str;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+
+		str = va_arg(ptr, char *);
+		if (str)
+			printf("%s", str);
+		if (str == NULL)
+			printf("nill");
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
