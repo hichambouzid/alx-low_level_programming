201a48d4b035270628382efc0b8d181cc0480278
hichambouzid <bouzidhicham51@gmail.com>
2023-03-31 19:49:20 -0700

    attribute constructor & destructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include<stdio.h>
+/**
+ * befor_main - how to excute a function beffor main
+ *
+ * Return: void
+ */
+void befor_main(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
