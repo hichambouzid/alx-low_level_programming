533ab5e775ad501e38ca98dbe98d233b9bcf1341
hichambouzid <bouzidhicham51@gmail.com>
2023-03-31 19:52:54 -0700

    attribute constructor & destructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,9 @@
  *
  * Return: void
  */
+void befor_main(void) __attribute__((constructor));
 void befor_main(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
