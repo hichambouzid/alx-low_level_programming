e78428254ec4743e7c42db82a67e96cf23f9b898
hichambouzid <bouzidhicham51@gmail.com>
2023-04-14 10:40:01 -0700

    creat a file if not exist and write unsigned them

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		i = 0;
 		while (text_content[i])
 			i++;
-		j = write(file, filename, i);
+		j = write(file, text_content, i);
 		if (j == -1)
 			return (-1);
 	}
