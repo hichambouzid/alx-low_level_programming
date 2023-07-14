21992c2075ae087e837b1d0bb84f5e7c4d912d3a
bouzid <bouzidhicham51@gmail.com>
2023-07-14 16:30:15 -0700

    assignment #1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include<unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, count,i,ret_w;
+	char buff[100];
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	i=0;
+	while (i<100)
+		buff[i]=0;
+	i=letters;
+	rd = -1;
+	count =0;
+	while (i && rd!=0)
+	{
+		rd = read(fd,buff,100);
+		if (rd != -1)
+			ret_w = write(1,buff,rd);
+		if (rd ==-1 ||ret_w!=rd)
+		{
+			close(fd);
+			return (0);
+		}
+		count +=rd;
+		i--;
+	}
+	close(fd);
+	return count;
+}
