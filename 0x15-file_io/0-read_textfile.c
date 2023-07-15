5ca02c3748a4d954428fd568e375738b9ff50323
bouzid <bouzidhicham51@gmail.com>
2023-07-14 17:49:48 -0700

    wef

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t rd, count,i,ret_w;
+	ssize_t rd, count, i, ret_w;
 	char buff[2];
 
 	if (!filename)
@@ -15,28 +15,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	i=0;
-	while (i<2)
+	i = 0;
+	while (i < 2)
 	{
-		buff[i]=0;
+		buff[i] = 0;
 		i++;
 	}
-	i=letters;
+	i = letters;
 	rd = -1;
-	count =0;
-	while (i>0 && (size_t)count<letters)
+	count = 0;
+	while (i > 0 && (size_t)count < letters)
 	{
-		rd = read(fd,buff,1);
-		if (rd==0)
-			break;
+		rd = read(fd, buff, 1);
+		if (rd == 0)
+			break ;
 		if (rd != -1)
-			ret_w = write(STDOUT_FILENO,buff,rd);
-		if (rd ==-1 ||ret_w!=rd)
+			ret_w = write(STDOUT_FILENO, buff, rd);
+		if (rd ==-1 || ret_w != rd)
 		{
 			close(fd);
 			return (0);
 		}
-		count +=rd;
+		count += rd;
 		i -= rd;
 	}
 	close(fd);
