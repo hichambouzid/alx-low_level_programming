c169730d581350a67bc413c763c26cf1fdaa8ad7
bouzid <bouzidhicham51@gmail.com>
2023-07-14 17:59:16 -0700

    fwef

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,8 @@
-#include<unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <stddef.h>
-#include "main.h"
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<sys/types.h>
+#include	<stddef.h>
+#include	"main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
