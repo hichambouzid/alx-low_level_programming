145ffb9e9c7f1abe5e9e28860983652d6ea3069a
bouzid <hbouzid1337@gmail.com>
2024-06-17 20:53:03 -0700

    fix ex00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	str[i] = 0;
-	if (write(1, str, i) < 0)
+	if (write(1, str, i) < 0 || i < (int)letters)
 	{
 		free(str);
 		return (0);
