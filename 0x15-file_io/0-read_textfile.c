c095846da46185b32d5d15fad6a3c672c846bd16
bouzid <hbouzid1337@gmail.com>
2024-06-17 20:45:37 -0700

    add ex00Wq

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile- function read from file name
+ * and print a specific number of
+ * letters.
+ *
+ * Return: if succes return deff 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	int fd;
+	char *str;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0 || !filename)
+		return (0);
+	str = malloc(sizeof(char) * (letters + 1));
+	if (!str)
+		return (0);
+	i = read(fd, str, letters);
+	if (i < 0)
+	{
+		free(str);
+		return (0);
+	}
+	str [i] = 0;
+	if (write(1, str, i) < 0)
+	{
+		free(str);
+		return (0);
+	}
+	return (i);
+}
